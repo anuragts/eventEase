26de8796e078e827e3f3303e85741d1416348fc9
Anurag30112003 <anuragsharma011011@gmail.com>
2023-03-25 05:17:15 +0530

    migrate home page

---
+++ src/pages/components/Loading.tsx
@@ -0,0 +1,10 @@
+const LineLoader = () => {
+
+  return (
+    <div className="text-lg p-10">
+        <div className="lds-facebook"><div></div><div></div><div></div></div>
+    </div>
+  );
+};
+
+export default LineLoader;
